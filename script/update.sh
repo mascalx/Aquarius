33cecfdd02503aa4d8af32e51d2ffb9867c46708
Mascal <carlomascellani@tiscali.it>
2017-04-17 11:24:10 +0200

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -4,9 +4,9 @@ cd /home/pi
 curl https://raw.githubusercontent.com/mascalx/Aquarius/master/script/acquario.py -o acquario.py
 
 cd /var/www
-wget https://raw.githubusercontent.com/mascalx/Aquarius/master/web/index.php -o index.php
-wget https://raw.githubusercontent.com/mascalx/Aquarius/master/web/data.php -o data.php
-wget https://raw.githubusercontent.com/mascalx/Aquarius/master/web/update.php -o update.php
-wget https://raw.githubusercontent.com/mascalx/Aquarius/master/web/style.css -o style.css
+curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/index.php -o index.php
+curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/data.php -o data.php
+curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/update.php -o update.php
+curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/style.css -o style.css
 
 sudo reboot
