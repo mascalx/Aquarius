528157e18e1c9f5c349261135634de3188b02f97
Mascal <carlomascellani@tiscali.it>
2017-06-04 18:54:36 +0200

    Update update.sh

--- script/update.sh
+++ script/update.sh
@@ -7,5 +7,6 @@ cd /var/www
 curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/index.php -o index.php
 curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/update.php -o update.php
 curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/style.css -o style.css
+curl https://raw.githubusercontent.com/mascalx/Aquarius/master/web/upgrade.php -o upgrade.php
 
 sudo reboot
