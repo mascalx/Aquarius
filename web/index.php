bff24cf4f664552f9f9e72077fa3126612e39907
Mascal <carlomascellani@tiscali.it>
2017-06-05 12:58:57 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -61,13 +61,15 @@ function GetLine($n){
     <div id="messaggio" style="color:yellow;" class="pclear"><br/></div>
     <iframe id="data" src="data.php"></iframe>
     <br/><br/>
-    <div class="maschera">
-        <b>Temperature ventilazione:</b><br/><br/>
-        Start: <input type="number" name="fan_start" min="0" max="35" onchange="SetLine(this.value,'1')" value=<?php echo GetLine(1); ?> /><br/>
-        Stop: <input type="number" name="fan_stop" min="0" max="35" onchange="SetLine(this.value,'2')" value=<?php echo GetLine(2); ?> /><br/><br/>
+    <div class="maschera" style="width:180px;">
+        <b>Ventilazione:</b><br/><br/>
+        Avvia: <input style="float:right;" type="number" name="fan_start" min="0" max="35" onchange="SetLine(this.value,'1')" value=<?php echo$
+        Ferma: <input style="float:right;" type="number" name="fan_stop" min="0" max="35" onchange="SetLine(this.value,'2')" value=<?php echo $
+    </div>
+    <div class="maschera" style="width:180px;">
         <b>Dosaggio cibo:</b><br/><br/>
-        <input style="width:110px;" type="time" name="lunch_time" onchange="SetLine(this.value,'12')" value=<?php echo GetLine(12); ?> /><br/>
-        <input style="width:110px;" type="time" name="elunch_time" onchange="SetLine(this.value,'13')" value=<?php echo GetLine(13); ?> /><br/>
+        Inizio: <input style="width:110px;height:32px;float:right;" type="time" name="lunch_time" onchange="SetLine(this.value,'12')" value=<?$
+        Fine: <input style="width:110px;height:32px;float:right;" type="time" name="elunch_time" onchange="SetLine(this.value,'13')" value=<?p$
     </div>
     <div class="pclear"><br/></div>
     <p style="margin-left:10px;"><b>Illuminazione</b></p>
